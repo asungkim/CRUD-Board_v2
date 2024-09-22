df408b24a98316a529520fe92fdc9fce6e6222f6
asungkim <asungkim007@naver.com>
2024-09-22 13:55:43 +0900

    post 라우터 작성 및 테스트 완료

--- server/index.js
+++ server/index.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const body_parser = require("body-parser");
+
 const mongoose = require("mongoose");
 require("dotenv").config();
+
+const authRouter = require("./routes/auth");
+const postsRouter = require("./routes/posts");
 
 const app = express();
 app.use(cors());
 app.use(body_parser.json());
 
-const authRouter = require("./routes/auth");
-app.use("/api/auth", authRouter);
-
 mongoose
   .connect(process.env.mongoose_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
+
+app.use("/api/auth", authRouter);
+app.use("/api/posts", postsRouter);
 
 app.get("/", (req, res) => {
   res.send("CRUD 서버 개설");
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
-const { auth } = require("../middleware/ auth");
+const { auth } = require("../middleware/auth");
 
 // 글 작성
 router.post("/", auth, async (req, res) => {
+  const { title, content } = req.body;
   try {
-    const { title, content } = req.body;
     const userId = req.user.id;
     const post = new Post({
       title,
@@ -36,7 +36,66 @@ router.get("/", auth, async (req, res) => {
 });
 
 // 특정 글 조회
-router.get("/:postId", auth, (req, res) => {});
+router.get("/:postId", auth, async (req, res) => {
+  try {
+    const findPost = await Post.findById(req.params.postId).populate(
+      "userId",
+      "username"
+    );
+    res.json(findPost);
+  } catch (err) {
+    res.json({ message: `Can't find specific Post` });
+  }
+});
 // 글 수정
+router.patch("/:postId", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId).populate(
+      "userId",
+      "username"
+    );
+    if (!post) return res.json({ message: "Post not found" });
+
+    const { title, content } = req.body;
+
+    console.log(post.userId.username);
+    console.log(req.user);
+    // 작성자만 수정 가능
+    if (post.userId.username === req.user.username) {
+      post.title = title;
+      post.content = content;
+
+      const updatedPost = await post.save();
+      res.json(updatedPost);
+    } else {
+      res.json({ message: `You don't have a permission` });
+    }
+  } catch (err) {
+    res.json({ message: "Failed to updated post" });
+  }
+});
 
 // 글 삭제
+router.delete("/:postId", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId).populate(
+      "userId",
+      "username role"
+    );
+    if (!post) return res.json({ message: "Post not found" });
+
+    if (
+      post.userId.username === req.user.username ||
+      post.userId.role === "admin"
+    ) {
+      await post.deleteOne();
+      res.json({ message: "Deleted post successfully" });
+    } else {
+      res.json({ message: `You don't have permission` });
+    }
+  } catch (err) {
+    res.json({ message: "Failed to delete post" });
+  }
+});
+
+module.exports = router;
