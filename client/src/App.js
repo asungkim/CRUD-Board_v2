78ca9a891c40c984a2a61a591d264d085842f03d
asungkim <asungkim007@naver.com>
2024-09-27 20:11:38 +0900

    로그인,회원가입, 로그아웃 구현 시작

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,24 @@ import "./App.css";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
+import { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const decodedUser = jwtDecode(token);
+      setUser({
+        id: decodedUser.id,
+        role: decodedUser.role,
+        username: decodedUser.username,
+      });
+    }
+  }, []);
+
   return (
     <Router>
       <Routes>
